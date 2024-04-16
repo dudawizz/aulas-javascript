d818cfa23b19fafc640ae440e4272819c03fcc28
dudawizz <langereduarda7@gmail.com>
2024-04-16 17:14:13 -0300

    exercicio de array

---
+++ js/02.arrays.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Declarando um array de alunos
+const alunos = ["Wilian", "Vinicius", "Marli", "Duda", "Gabriel"];
+
+// Exibindo a estrutura do array (somente no console)
+console.log(alunos);
+
+// Acessando individualmente alguns alunos
+console.log(alunos[1]); // Vinicius
+console.log(alunos[3]); // Duda
+console.log(alunos[0]); // Wilian
+
+/* Exercício */
+const favoritos = ["Volei", "Rock", "BlackSabbath", "Viajar", "Pintar", "Desenhar", "Verde", "Pão de Mel"]
+
+console.log(favoritos);
+
+console.log(favoritos[2]);
+console.log(favoritos[5]);
+console.log(favoritos[7]);
+
+console.log(`Minha banda de rock favorita é ${favoritos[2]}`);
+console.log(`Minha maior paixão é ${favoritos[5]}`);
+console.log(`Meu doce favorito é ${favoritos[7]}`);
+
