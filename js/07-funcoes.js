10691349f6a773b90b08f519553dadd4d6f4c50d
dudawizz <langereduarda7@gmail.com>
2024-04-23 17:24:52 -0300

    tipos de funções

--- js/07-funcoes.js
+++ js/07-funcoes.js
@@ -58,3 +58,52 @@ function formatarValor(valor){
 console.log(`Preço: ${formatarValor(preco)}`);
 console.log(`Desconto: ${formatarValor(desconto)}`);
 console.log(`Preço final: ${formatarValor(precoFinal)}`);
+
+console.log("..................");
+
+/* Sobre a sintaxe 3: Arrow Function 
+Sintaxe moderna para funções no JavaScript, bastante usada em biblioteca/framework como React, React Native, Angular, Vue e etc.
+
+A sintaxe getal é:
+
+const algumNome = () => ();
+
+No entanto, esta sintaxe pode ser simplificada e, alguns casos.
+*/
+
+// Versão 1 (nomeada/declarada)
+/* function dobra(valor){
+    return valor *2;
+}; */
+
+// Versão 2 (arrow function)
+/* const dobra = (valor) => {
+    return valor *2;
+} */
+
+// Versão 3 (arrow function com retorno implícito)
+const dobra = (valor) => valor *2;
+
+
+// Chamada
+console.log(dobra(65));
+console.log(dobra(486));
+console.log(dobra(64));
+
+console.log("...............");
+
+// Funções como métodos/ comportamentos de Objetos
+let pessoa = {
+    nome: "Fulano",
+    idade: 25,
+    verificaIdade(){
+        if(pessoa.idade >= 18){
+            return "maior";
+        } else {
+            return "menor"
+        }
+    }
+}
+
+console.log(pessoa.nome);
+console.log(`É ${pessoa.verificaIdade()}`);
