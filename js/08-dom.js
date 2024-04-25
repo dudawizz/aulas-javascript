95000069ca9eb5732d39fd3756e74f80a9b947de
dudawizz <langereduarda7@gmail.com>
2024-04-25 14:20:45 -0300

    Percorrer o array de listaDeEditores

--- js/08-dom.js
+++ js/08-dom.js
@@ -1,47 +1,38 @@
-'use script'
+'use strict';
 
-/* Funções ou métodos mais comuns para seleção de elementos no DOM
+/* Funções/métodos mais comuns para seleção de elementos no DOM 
 
--getElementById() (apenas um elemento)
-Selecionar um elemento através do atributo ID
+- getElementById()
+Seleciona UM ELEMENTO através do atributo ID
 
--querySelector()
-Seleciona UM ELEMENTO também através de seletor na lógica CSS
+- querySelector()
+Seleciona UM ELEMENTO através de seletor baseado na lógica CSS
 
--querySelectorAll()
-Seleciona VÁRIOS ELEMENTOS também através de seletor(es) na lógica CSS
+- querySelectorAll()
+Seleciona VÁRIOS ELEMENTOS através de seletor(es) baseado na lógica CSS */
 
-*/
-
-// Exemplo 1: getElementyById()
+// Exemplo 1: getElementById()
 const legenda = document.getElementById("legenda");
 console.log(legenda);
 
 // Exemplo 2: querySelector()
-const titulo = document.querySelector("h1")
+const titulo = document.querySelector("h1");
 console.log(titulo);
 
 const sobreFront = document.querySelector("#sobre");
 console.log(sobreFront);
 
-/* Mini-exercício
-1)Faça o acesso/seleção do h2 de Back-End e mostre no console.
-
-2)Faça o acesso/seleção apenas no h2 existente dentro da div com a classe 'editores' e mostre no console.
-*/
-
+/* Mini-exercícios
+1) Faça o acesso/seleção do h2 de Back-End e mostre no
+console. */
 const backEnd = document.querySelector(".back-end");
 console.log(backEnd);
 
-/* tag: {}
-classe: .
-id: #
-*/
-
+/* 2) Faça o acesso/seleção apenas do h2 existente dentro da div com a classe 'editores' e mostre no console. */
 const codigo = document.querySelector(".editores h2");
 console.log(codigo);
 
-// Exemplos 3: querySelectorAll() obs: parecido com um array
+// Exemplo 3: querySelectorAll()    - obs: funciona parecido com array
 const subtitulos = document.querySelectorAll("h2");
 console.log(subtitulos);
 console.log(subtitulos[0]);
@@ -50,32 +41,51 @@ const variosElementos = document.querySelectorAll("p, a, p b, #teste, .xyz");
 console.log(variosElementos);
 
 /* Modificando o DOM:
-Podemos alterar conteúdo, tags, estilos, criar elementos, remover elementos etc
-*/
+Podemos alterar conteúdo, tags, estilos, criar elementos,
+remover elementos etc */
 
-// Alteração do conteúdo textual
-legenda.textContent = "Imagem do modelo de objetos do documento"
+// Alteração de conteúdo textual
+legenda.textContent = "Imagem do modelo de objetos do documento";
 
+// Alteração de conteúdo com suporte à tags
 sobreFront.innerHTML = "<i>Front-End</i>";
 
-// Alterações de estilos inline (CSS in JS)
-backEnd.style.color = "red"
-backEnd.style.textAlign = "center"
+// Alteração de estilos inline (CSS in JS)
+backEnd.style.color = "red";
+backEnd.style.textAlign = "center";
 
 // Aplicação de classes CSS via JavaScript
-titulo.classList.add("destaque")
+titulo.classList.add("destaque");
 
 const listaDeEditores = [
     {
         nome: "Visual Studio Code",
-        finalidade: "Desenvolvimento Web em geral"
-    }
+        finalidade: "Desenvolvimento web em geral"
+    },
     {
         nome: "Android Studio",
         finalidade: "Desenvolvimento de aplicações Android"
-    }
+    },
     {
         nome: "Xcode",
-        finalidade: "Desenvolvimento de aplicações IOS"
+        finalidade: "Desenvolvimento de aplicações iOS"
     }
-]
+];
+
+// Acessando/selecionando a lista que será usada no HTML
+const lista = document.querySelector (".lista");
+console.log(lista);
+
+/* Percorrer o array de listaDeEditores, criar os itens <li> com o conteúdo do array e adicioná-los à lista HTML. */
+
+/* for (variavel/constante de acesso of arrayComDadosQueQueremosAcessar) {} */
+for(const editor of listaDeEditores){
+    // Etapa 1: criar elemento <li>
+    const item = document.createElement("li");
+
+    // Etapa 2: adicionar conteúdo ao novo elemento
+    item.innerHTML = `<b>${editor.nome}</b> ${editor.finalidade}`
+
+    // Etapa 3: adicionar o elemento à lista <ol>
+    lista.appendChild(item);
+};
